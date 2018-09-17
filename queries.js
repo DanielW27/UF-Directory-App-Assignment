00b13b7c7af792d9dd0af24936e7acaf804286b2
Daniel Williamson <32105320+DanielW27@users.noreply.github.com>
2018-09-17 18:25:46 -0400

    changes to queries

--- queries.js
+++ queries.js
@@ -6,56 +6,45 @@ var fs = require('fs'),
     config = require('./config'),
     jsonListings = require('./listings.json');
 
+
 var findLibraryWest = function() {
-  /*
-    Find the document that contains data corresponding to Library West,
-    then log it to the console.
-   */
-   Listing.find({name: 'Library West'}, function(err, listing){
-     if(err) throw err;
-     console.log(listing);
-   });
-};
+       Listing.find({name: 'Library West'}, function(err, listing){
+         if(err) throw err;
+         console.log(listing);
+       });
+    };
+
 var removeCable = function() {
+      Listing.find({code: 'CABL'}, function(err,listing){
+        if(err) throw err;
+        console.log(listing);
+      });
+      Listing.findOneAndRemove({code: 'CABL'}, function(err){
+        if (err) throw err;
+      });
+    };
 
-  Listing.find({code: 'CABL'}, function(err,listing){
-    if(err) throw err;
-    console.log(listing);
-    listing.remove(function(err){
-      if (err) throw err;
-    });
-  });
-
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console.
-   */
-};
 var updatePhelpsLab = function() {
-  /*
-    Phelps Laboratory's address is incorrect. Find the listing, update it, and then
-    log the updated document to the console.
-   */
-   Listing.find({code: 'PHL'}, function(err,listing){
-     if(err) throw err;
-     listing.address = '1953 Museum Rd, Gainesville, FL 32603';
-     listing.save(function(err){
-       if (err) throw err;
-     });
-   });
-};
-var retrieveAllListings = function() {
-  /*
-    Retrieve all listings in the database, and log them to the console.
-   */
-   Listing.find({}, function(err,listings){
+    Listing.findOneAndUpdate({ code: 'PHL' }, { address: '1953 Museum Rd, Gainesville, FL 32603' }, function(err, user) {
      if (err) throw err;
-     console.log(listings);
-   });
+     console.log(user);
+    });
 };
 
-findLibraryWest();
-removeCable();
-updatePhelpsLab();
-retrieveAllListings();
+var retrieveAllListings = function() {
+       Listing.find({}, function(err,listings){
+         if (err) throw err;
+         console.log(listings);
+       });
+    };
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+console.log("hello");
+conn.once('open', () =>{
+  console.log("we're on");
+  findLibraryWest();
+  removeCable();
+  updatePhelpsLab();
+  retrieveAllListings();
+});
